83d8f8347c416a95160a70e0401b96abf96f5770
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-02-10 17:19:16 +0700

    very hard

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,47 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+
+
+void showData(double *data,int N ,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << data[i];
+        if((i+1)%M==0) cout << "\n" ;
+        else cout << " ";
+    }
+}
+
+
+void randData(double *data,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        data[i] = (rand()%101)*0.01;
+    }
+}
+
+void findRowSum(const double *data,double *sum1,int N,int M){
+
+for(int i=0;i <N ;i++){
+double sum =0 ;
+ for(int j=0;j <M ; j++){
+  sum+=*(data+j+(i*M));
+
+}
+ *(sum1+i)=sum ;
+}
+}
+
+void findColSum(const double *data,double *sum2,int N,int M){
+
+for(int i=0;i <M ;i++){
+double sum =0 ;
+ for(int j=0;j <N ; j++){
+  sum+=*(data+i+(j*M));
+
+}
+ *(sum2+i)=sum ;
+}
+}
+
+
